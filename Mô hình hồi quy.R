601335689c57f256c8b6125e8065d69d921c3195
Thu Hương Orianna <79627990+thuhuong17@users.noreply.github.com>
2022-12-26 12:00:30 +0700

    Update Mô hình hồi quy.R

--- Mô hình hồi quy.R
+++ Mô hình hồi quy.R
@@ -5,3 +5,11 @@ plot(data_re$`employment_rate_ft_perm` ~ data_re$basic_monthly_median)
 
 cor.test(data_re$`employment_rate_ft_perm`, data_re$basic_monthly_median)
 cor.test(data_re$`employment_rate_ft_perm`, data_re$basic_monthly_median, method = "spearman")
+cor.test(data_re$`employment_rate_ft_perm`, data_re$basic_monthly_median, method = "kendall")
+
+hoiquy <- lm(data_re$`basic_monthly_median` ~ data_re$employment_rate_ft_perm)
+fitted(hoiquy)
+resid(hoiquy)
+#vẽ biểu đồ kiểm định giả định 
+datten <- par(mfrow = c(2,2))
+plot(hoiquy)
